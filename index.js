004e822dd96e9d5dadad1a9c632c1317b5af7ff8
ChibikeEchendu1 <32308443+ChibikeEchendu1@users.noreply.github.com>
2019-05-13 11:59:53 -0400

    jgyjgj

--- index.js
+++ index.js
@@ -21,16 +21,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 require('./routes/authRoutes')(app);
 
-if (process.env.NODE_ENV === "production") {
+ if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
     const path = require('path');
 
-    app.get('/',(req,res) => {
+    app.get('*',(req,res) => {
         res.sendFile(path.resolve(__dirname,'client','build','index.html'));
 
     });
-}
+} 
 
 
 
